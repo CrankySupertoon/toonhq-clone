4f570942c673f6e910767fa705a6a570d3655808
garb <garb@steam.cat>
2016-12-14 14:06:54 +1100

    Finished the HTTP server
    Built in Nodejs and Express v4

---
+++ server.js
@@ -0,0 +1,41 @@
+// Main Express Router -- ToonHQ Clone
+// Created by @gaarb
+
+// Modules
+
+var express = require('express');
+var app = express();
+var bp = require('body-parser');
+var morgan = require('morgan');
+
+// Configuration
+
+var port = process.env.PORT || 8080; // Designating the port.
+var router = express.Router(); // Allowing us to create our routes.
+
+app.use('/api/v1/', router); // Registering the routes.
+
+app.use(morgan('dev'));
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json());
+
+// Middleware -- This is used so when we know if an infected connection is incoming.
+
+router.use(function(req, res, next){
+  // Advanced logging feature.
+  console.log('[Middleware] There is an incoming connection coming from: ' + req.connection + '.');
+  next();
+});
+
+// Routers
+
+router.get('/', function(req, res) {
+  res.json(
+    { message: "Welcome to ToonHQ Clone API!" }
+  );
+});
+
+// Finalise
+
+app.listen(port);
+console.log('[Server.js] Web-server has successfully started!');
