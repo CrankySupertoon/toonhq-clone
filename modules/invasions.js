630278042dfefa862d048f1f440056221991ef01
garb <garb@steam.cat>
2016-12-14 15:29:27 +1100

    Removed logging of InvasionData.
    It was used for debugging reasons.

--- modules/invasions.js
+++ modules/invasions.js
@@ -28,7 +28,6 @@ function grabInvasionList() {
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       InvasionData = body;
-      console.log(InvasionData);
     }
   });
 }
