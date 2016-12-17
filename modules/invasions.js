f499ea2c05d17e5602f4209438ed7b627fe04ba6
garb <garb@steam.cat>
2016-12-17 20:46:52 +1100

    Commit semi-working Invasion module

--- modules/invasions.js
+++ modules/invasions.js
@@ -15,6 +15,10 @@ var loop = new looper;
 
 var InvasionURL = "https://toontownrewritten.com/api/invasions"; // URL from Toontown Rewritten Data.
 var InvasionData = []; // Array for Invasion Data.
+var currentInvasions = []; // The current Invasions go in here.
+var InvasionSchema = { 'id': '', 'district': '', 'type': '', 'progress': '', 'asOf': '' }; // Schema for adding data to InvasionData.
+var RemoveSchema = { 'district': '' };
+var RemoveInvasion = []; // List to remove invasions that aren't active.
 
 // Router Functions
 
@@ -26,13 +30,75 @@ module.exports = {
 
 // Normal Functions
 
+function removeInvasion (array, delList) {
+  for(var i = 0; i < array.length; i++) {
+    var obj = array[i];
+
+    if(delList.indexOf(obj.district) !== -1) {
+        arrayOfObjects.splice(i, 1);
+        i--;
+    }
+  }
+}
+
+function organiseInvasions () {
+  var i = 0;
+  var curInv = currentInvasions.invasions;
+  for (var district in curInv) {
+    if (curInv.hasOwnProperty(district)) {
+      i++;
+      var keyFound = false;
+      for (var key in InvasionData) {
+        if (InvasionData.hasOwnProperty(key)) {
+          if (InvasionData[key].district === district) {
+            InvasionData[key].type = curInv[district].type;
+            InvasionData[key].progress = curInv[district].progress;
+            InvasionData[key].asOf = curInv[district].asOf;
+            keyFound = true;
+            break;
+          }
+        }
+      }
+      if (keyFound) { continue; }
+      InvasionSchema = {};
+      InvasionSchema.id = i;
+      InvasionSchema.district = district;
+      InvasionSchema.type = curInv[district].type;
+      InvasionSchema.progress = curInv[district].progress;
+      InvasionSchema.asOf = curInv[district].asOf;
+      InvasionData.push(InvasionSchema);
+    }
+  }
+  // Remove leftover invasions
+  for (var key in InvasionData) {
+    if (InvasionData.hasOwnProperty(key)) {
+      var foundInvasion = false;
+      RemoveSchema = {};
+      for (var district in curInv) {
+        if (curInv.hasOwnProperty(district)) {
+          if (InvasionData[key].district === district) {
+            foundInvasion = true;
+            break;
+          }
+        }
+      }
+      if (!foundInvasion) {
+        delete currentInvasions[district];
+        console.log('Deleted', district);
+      }
+    }
+  }
+}
+
 function grabInvasionList() {
   request({
     url: InvasionURL,
     json: true
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      InvasionData = body; // Puts the body data into the InvasionData array.
+      currentInvasions = body; // Puts the body data into the InvasionData array.
+    } else if (body.invasions == {}) {
+       InvasionData = { 'banner': 'There are currently no invasions.' };
     } else {
       // Detection if API is down.
       InvasionData = { 'banner': 'Toontown Rewritten API is currently down.' };
@@ -40,9 +106,16 @@ function grabInvasionList() {
   });
 }
 
+function execute() {
+  grabInvasionList();
+  setTimeout(function () {
+    organiseInvasions();
+  }, 3000);
+}
+
 // Looping functions -- I don't want to depend on the front-end pinging the server incase it breaks, plus others might like to use it.
 
-loop.add(grabInvasionList, []); // Adding the function 'grabInvasionList' to the looper.
+loop.add(execute, []); // Adding the function 'grabInvasionList' to the looper.
 loop.setInterval(5000); // Every 5 seconds it'll update the list.
 
 loop.run(); // Finally running the function.
